73a6cd06539b3319a4eeab2a01a66d4304a99160
书耘 <zwy174365@koubei.com>
2018-12-25 18:16:42 +0800

    唤醒、语音识别功能代码

--- src/main/java/com/iflytek/mscv5plusdemo/WakeDemo.java
+++ src/main/java/com/iflytek/mscv5plusdemo/WakeDemo.java
@@ -3,11 +3,15 @@ package com.iflytek.mscv5plusdemo;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,6 +24,10 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.RequestListener;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
@@ -29,8 +37,11 @@ import com.iflytek.cloud.VoiceWakeuper;
 import com.iflytek.cloud.WakeuperListener;
 import com.iflytek.cloud.WakeuperResult;
 import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.iflytek.cloud.util.ResourceUtil;
 import com.iflytek.cloud.util.ResourceUtil.RESOURCE_TYPE;
+import com.iflytek.speech.setting.IatSettings;
+import com.iflytek.speech.util.JsonParser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -58,13 +69,15 @@ public class WakeDemo extends Activity implements OnClickListener {
     private String keep_alive = "1";
     private String ivwNetMode = "0";
     //private IvwActivity activity;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.wake_activity);
-
+        mSharedPreferences = getSharedPreferences(IatSettings.PREFER_NAME, Activity.MODE_PRIVATE);
+        mContext = getApplicationContext();
         initUi();
         // 初始化唤醒对象
         mIvw = VoiceWakeuper.createWakeuper(this, null);
@@ -250,12 +263,15 @@ public class WakeDemo extends Activity implements OnClickListener {
                 buffer.append("\n");
                 buffer.append("【尾端点】" + object.optString("eos"));
                 resultString = buffer.toString();
+                startIat(mContext);
+
             } catch (JSONException e) {
                 resultString = "结果解析出错";
                 e.printStackTrace();
             }
             textView.setText(resultString);
-
+            textView.append("\n \n 唤醒成功……\n \n");
+            textView.append("\n 开始语音听写识别……");
             //Ωactivity.wakeUpAndUnlock();
         }
 
@@ -322,5 +338,244 @@ public class WakeDemo extends Activity implements OnClickListener {
                 findViewById(R.id.seekBar_thresh).setEnabled(enabled);
             }
         });
+    }
+
+    /**
+     * 启动语音听写
+     */
+    private void startIat(Context context) {
+        // 初始化识别无UI识别对象
+        // 使用SpeechRecognizer对象，可根据回调消息自定义界面；
+        mIat = SpeechRecognizer.createRecognizer(context, mInitListener);
+
+        // 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
+        // 使用UI听写功能，请根据sdk文件目录下的notice.txt,放置布局文件和图片资源
+        mIatDialog = new RecognizerDialog(this, mInitListener);
+        // 设置参数
+        setParam();
+        boolean isShowDialog = mSharedPreferences.getBoolean(getString(R.string.pref_key_iat_show), true);
+        if (isShowDialog) {
+            // 显示听写对话框
+            mIatDialog.setListener(mRecognizerDialogListener);
+            mIatDialog.show();
+//            showTip(getString(R.string.text_begin));
+            textView.append(getString(R.string.text_begin));
+        } else {
+            // 不显示听写对话框
+            ret = mIat.startListening(mRecognizerListener);
+            if (ret != ErrorCode.SUCCESS) {
+                showTip("听写失败,错误码：" + ret);
+            } else {
+                showTip(getString(R.string.text_begin));
+            }
+        }
+    }
+
+
+    /**
+     * 听写监听器。
+     */
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+            showTip("开始说话");
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            // Tips：
+            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+            if (mTranslateEnable && error.getErrorCode() == 14002) {
+                showTip(error.getPlainDescription(true) + "\n请确认是否已开通翻译功能");
+            } else {
+                showTip(error.getPlainDescription(true));
+            }
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+            showTip("结束说话");
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+            if (mTranslateEnable) {
+                printTransResult(results);
+            } else {
+                String text = JsonParser.parseIatResult(results.getResultString());
+//                textView.append(text);
+//                textView.setSelection(textView.length());
+            }
+
+            if (isLast) {
+                //TODO 最后的结果
+            }
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+            showTip("当前正在说话，音量大小：" + volume);
+            Log.d(TAG, "返回音频数据：" + data.length);
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            //		Log.d(TAG, "session id =" + sid);
+            //	}
+        }
+    };
+
+    int ret = 0;// 函数调用返回值
+
+    // 语音听写对象
+    private SpeechRecognizer mIat;
+
+    // 语音听写UI
+    private RecognizerDialog mIatDialog;
+
+    private SharedPreferences mSharedPreferences;
+
+    // 默认识别模式为云端
+    private String mEngineType = "cloud";
+
+    private boolean mTranslateEnable = false;
+
+
+    /**
+     * 听写UI监听器
+     */
+    private RecognizerDialogListener mRecognizerDialogListener = new RecognizerDialogListener() {
+        public void onResult(RecognizerResult results, boolean isLast) {
+            Log.d(TAG, "recognizer result：" + results.getResultString());
+
+            if (mTranslateEnable) {
+                textView.append("\n 获得语音翻译结果：\n");
+                printTransResult(results);
+            } else {
+                String text = JsonParser.parseIatResult(results.getResultString());
+                textView.append("\n 获得语音翻译结果：\n");
+                textView.append("\n" + text);
+//                mResultText.setSelection(mResultText.length());
+            }
+        }
+
+        /**
+         * 识别回调错误.
+         */
+        public void onError(SpeechError error) {
+            if (mTranslateEnable && error.getErrorCode() == 14002) {
+                showTip(error.getPlainDescription(true) + "\n请确认是否已开通翻译功能");
+            } else {
+                showTip(error.getPlainDescription(true));
+            }
+        }
+
+    };
+
+
+    private void printTransResult(RecognizerResult results) {
+        String trans = JsonParser.parseTransResult(results.getResultString(), "dst");
+        String oris = JsonParser.parseTransResult(results.getResultString(), "src");
+
+        if (TextUtils.isEmpty(trans) || TextUtils.isEmpty(oris)) {
+            showTip("解析结果失败，请确认是否已开通翻译功能。");
+        } else {
+            textView.append("原始语言:\n" + oris + "\n目标语言:\n" + trans);
+        }
+
+    }
+
+    /**
+     * 初始化监听器。
+     */
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            Log.d(TAG, "SpeechRecognizer init() code = " + code);
+            if (code != ErrorCode.SUCCESS) {
+                showTip("初始化失败，错误码：" + code);
+            }
+        }
+    };
+
+
+    /**
+     * 参数设置
+     *
+     * @return
+     */
+    public void setParam() {
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+        String lag = mSharedPreferences.getString("iat_language_preference", "mandarin");
+        // 设置引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+
+        this.mTranslateEnable = mSharedPreferences.getBoolean(this.getString(R.string.pref_key_translate), false);
+        if (mEngineType.equals(SpeechConstant.TYPE_LOCAL)) {
+            // 设置本地识别资源
+            mIat.setParameter(ResourceUtil.ASR_RES_PATH, getResourcePath());
+        }
+        if (mEngineType.equals(SpeechConstant.TYPE_CLOUD) && mTranslateEnable) {
+            Log.i(TAG, "translate enable");
+            mIat.setParameter(SpeechConstant.ASR_SCH, "1");
+            mIat.setParameter(SpeechConstant.ADD_CAP, "translate");
+            mIat.setParameter(SpeechConstant.TRS_SRC, "its");
+        }
+        //设置语言，目前离线听写仅支持中文
+        if (lag.equals("en_us")) {
+            // 设置语言
+            mIat.setParameter(SpeechConstant.LANGUAGE, "en_us");
+            mIat.setParameter(SpeechConstant.ACCENT, null);
+
+
+            if (mEngineType.equals(SpeechConstant.TYPE_CLOUD) && mTranslateEnable) {
+                mIat.setParameter(SpeechConstant.ORI_LANG, "en");
+                mIat.setParameter(SpeechConstant.TRANS_LANG, "cn");
+            }
+        } else {
+            // 设置语言
+            mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+            // 设置语言区域
+            mIat.setParameter(SpeechConstant.ACCENT, lag);
+
+            if (mEngineType.equals(SpeechConstant.TYPE_CLOUD) && mTranslateEnable) {
+                mIat.setParameter(SpeechConstant.ORI_LANG, "cn");
+                mIat.setParameter(SpeechConstant.TRANS_LANG, "en");
+            }
+        }
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, mSharedPreferences.getString("iat_vadbos_preference", "4000"));
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, mSharedPreferences.getString("iat_vadeos_preference", "1000"));
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, mSharedPreferences.getString("iat_punc_preference", "1"));
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+    }
+
+    private String getResourcePath() {
+        StringBuffer tempBuffer = new StringBuffer();
+        //识别通用资源
+        tempBuffer.append(ResourceUtil.generateResourcePath(this, ResourceUtil.RESOURCE_TYPE.assets, "asr/common.jet"));
+        tempBuffer.append(";");
+        tempBuffer.append(ResourceUtil.generateResourcePath(this, ResourceUtil.RESOURCE_TYPE.assets, "asr/sms.jet"));
+        //识别8k资源-使用8k的时候请解开注释
+        return tempBuffer.toString();
     }
 }
